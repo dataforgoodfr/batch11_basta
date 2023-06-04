86d12b27c381ac116d45961ecfecf3910cc034bb
Augustin BAR <baraugustin26@gmail.com>
2023-06-04 18:38:28 +0200

    Formatting code

--- discord_bot/extensions/forumManager.py
+++ discord_bot/extensions/forumManager.py
@@ -1,32 +1,35 @@
 # CE FICHIER CONTIENT LA CLASSE MAÎTRESSE DU CHATBOT
 
-from discord.ext import tasks, commands
-
 import json
 from os.path import exists
 from shutil import copyfile
-from datetime import timezone, time
 
-__all__ = ["forumManager"]
+from discord.ext import commands
 
-ACTIVE_FORUMS = {} # Ce dictionnaire fait le lien entre 'ID du serveur' -> 'Objet Forum associé'
+__all__ = ["ForumManager"]
+
+# Ce dictionnaire fait le lien entre 'ID du serveur' -> 'Objet Forum associé'
+ACTIVE_FORUMS = {}
+
 
 def get_forums():
     return ACTIVE_FORUMS
 
+
 def get_forum(server_id: int):
-    return ACTIVE_FORUMS[server_id] if server_id in ACTIVE_FORUMS.keys() else None
-    
+    return (
+        ACTIVE_FORUMS[server_id] if server_id in ACTIVE_FORUMS.keys() else None
+    )
+
+
 # Un objet Forum par serveur
-class Forum():
-    
+class Forum:
     def __init__(self, bot: commands.Bot, server_id: int):
         self.bot = bot
         self.server_id = server_id
 
     def load_config(self):
-
-        config_filename = "./configurations/"+str(self.server_id)+".json"
+        config_filename = "./configurations/" + str(self.server_id) + ".json"
 
         if not exists(config_filename):
             copyfile("./configurations/template.json", config_filename)
@@ -37,7 +40,7 @@ class Forum():
     def get_config(self):
         return self.CONFIG
 
-    def generate(bot: commands.Bot, server_id: int): # FACTORY METHOD
+    def generate(bot: commands.Bot, server_id: int):  # FACTORY METHOD
         forum = Forum(bot, server_id)
         ACTIVE_FORUMS[server_id] = forum
 
@@ -48,39 +51,61 @@ class Forum():
 
 # Un objet pour le bot, chargé de gérer les objets Forum
 class ForumManager(commands.Cog):
-
     async def reload(self):
         async for guild in self.bot.fetch_guilds():
             if guild.id not in ACTIVE_FORUMS.keys():
-                Forum.generate(self.bot, guild.id) # Voir si passer seulement l'ID suffit où si il y a un gain à passer l'objet guild entièrement 
+                # Voir si passer seulement l'ID suffit où si il y a un gain à
+                # passer l'objet guild entièrement
+                Forum.generate(self.bot, guild.id)
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.hybrid_command(name="load_forums", description="Génère des objets forums et des fichiers de configuration pour les serveurs qui n'en auraient pas.")
+    @commands.hybrid_command(
+        name="load_forums",
+        description="Génère des objets forums et des fichiers de \
+            configuration pour les serveurs qui n'en auraient pas.",
+    )
     async def load_configs(self, ctx: commands.Context):
         if not ctx.message.author.guild_permissions.administrator:
-            await ctx.interaction.response.send_message("Vous n'avez pas le droit d'utiliser cette commande.", ephemeral=True, delete_after=10)
+            await ctx.interaction.response.send_message(
+                "Vous n'avez pas le droit d'utiliser cette commande.",
+                ephemeral=True,
+                delete_after=10,
+            )
             return
         await self.reload()
         await ctx.send("Done!", delete_after=5)
 
-    @commands.hybrid_command(name="reload_config", description="Recharge la configuration en fonction du contenu du fichier JSON associé.")
+    @commands.hybrid_command(
+        name="reload_config",
+        description="Recharge la configuration en fonction du contenu \
+            du fichier JSON associé.",
+    )
     async def reload_config(self, ctx: commands.Context):
         if not ctx.message.author.guild_permissions.administrator:
-            await ctx.interaction.response.send_message("Vous n'avez pas le droit d'utiliser cette commande.", ephemeral=True, delete_after=10)
+            await ctx.interaction.response.send_message(
+                "Vous n'avez pas le droit d'utiliser cette commande.",
+                ephemeral=True,
+                delete_after=10,
+            )
             return
         if ctx.guild.id in ACTIVE_FORUMS.keys():
             forum = ACTIVE_FORUMS[ctx.guild.id]
             forum.load_config()
             await ctx.send("Done!", delete_after=5)
         else:
-            await ctx.send("Erreur: pas d'objet Forum pour ce serveur - pas de configuration associée ; utilisez _/load_configs_.")
+            await ctx.send(
+                "Erreur: pas d'objet Forum pour ce serveur - \
+                pas de configuration associée ; utilisez _/load_configs_."
+            )
 
-    # Lorsque le bot rejoint un nouveau serveur -> créer un objet Forum (et une configuration associée)
+    # Lorsque le bot rejoint un nouveau serveur
+    # -> créer un objet Forum (et une configuration associée)
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         Forum.generate(self.bot, guild.id)
+
 
 async def setup(bot) -> None:
     manager = ForumManager(bot)
