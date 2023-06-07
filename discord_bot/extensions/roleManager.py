051f4780b778714152da0d6040ef8a0baead8965
ArthusWQZ <arthus.wauquiez@neuf.fr>
2023-06-07 18:00:13 +0200

    Fixed a bug where reacting to a message prior to the bot's session did not trigger the event

--- discord_bot/extensions/roleManager.py
+++ discord_bot/extensions/roleManager.py
@@ -11,14 +11,15 @@ class RoleManager(commands.Cog):
     bot: commands.Bot
 
     @commands.Cog.listener()
-    async def on_reaction_add(self, reaction: discord.Reaction, user: discord.Member):
-        forum = self.bot.get_cog("ForumManager").get_forum(reaction.message.guild.id)
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        forum = self.bot.get_cog("ForumManager").get_forum(payload.guild_id)
         config = forum.config
-        if reaction.message.id == config["ROLE_MANAGER"]["RULE_MESSAGE_ID"]:
-            if reaction.emoji == "✅":
-                base_role = reaction.message.guild.get_role(config["ROLE_MANAGER"]["BASE_ROLE_ID"])
-                if base_role not in user.roles:
-                    user.add_roles()
+        if payload.message_id == config["ROLE_MANAGER"]["RULE_MESSAGE_ID"]:
+            if str(payload.emoji) == "✅":
+                guild = self.bot.get_guild(payload.guild_id)
+                base_role = guild.get_role(config["ROLE_MANAGER"]["BASE_ROLE_ID"])
+                if base_role not in payload.member.roles:
+                    await payload.member.add_roles(base_role)
 
 
 async def setup(bot) -> None:
