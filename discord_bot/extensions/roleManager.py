4e236999d30e61d4ff42409223f424c4c17c4b6d
ArthusWQZ <arthus.wauquiez@neuf.fr>
2023-06-07 17:51:35 +0200

    Implemented role giving when reacting on rule message

--- discord_bot/extensions/roleManager.py
+++ discord_bot/extensions/roleManager.py
@@ -10,7 +10,15 @@ __all__ = ["roleManager"]
 class RoleManager(commands.Cog):
     bot: commands.Bot
 
-    
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction: discord.Reaction, user: discord.Member):
+        forum = self.bot.get_cog("ForumManager").get_forum(reaction.message.guild.id)
+        config = forum.config
+        if reaction.message.id == config["ROLE_MANAGER"]["RULE_MESSAGE_ID"]:
+            if reaction.emoji == "✅":
+                base_role = reaction.message.guild.get_role(config["ROLE_MANAGER"]["BASE_ROLE_ID"])
+                if base_role not in user.roles:
+                    user.add_roles()
 
 
 async def setup(bot) -> None:
