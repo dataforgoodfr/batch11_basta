36e250ce22a218ad14d711e58e7f408a8d2db1db
Augustin BAR <baraugustin26@gmail.com>
2023-06-08 10:10:07 +0200

    adding polls commands

--- discord_bot/extensions/forumManager.py
+++ discord_bot/extensions/forumManager.py
@@ -5,6 +5,7 @@ from os.path import exists
 from shutil import copyfile
 from typing import Tuple
 
+import discord
 import modules.AnnouncementModule as AnnouncementModule
 from discord.ext import commands
 
@@ -152,6 +153,102 @@ class ForumManager(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         await Forum.generate(self.bot, guild.id)
+
+    @commands.hybrid_command(
+        name="poll",
+        description="Lance un sondage",
+    )
+    async def poll(self, ctx, multivote: bool, question, options: str):
+        options = options.split(",")
+        if len(options) <= 1:
+            await ctx.send("You need more than one option to make a poll!")
+            return
+        if len(options) > 10:
+            await ctx.send("You cannot make a poll for more than 10 things!")
+            return
+
+        if len(options) == 2 and (
+            (options[0].lower, options[1].lower) == ("oui", "non")
+            or (options[0].lower, options[1].lower) == ("non", "oui")
+        ):
+            reactions = ["✅", "❌"]
+        else:
+            reactions = [
+                "1️⃣",
+                "2️⃣",
+                "3️⃣",
+                "4️⃣",
+                "5️⃣",
+                "6️⃣",
+                "7️⃣",
+                "8️⃣",
+                "9️⃣",
+                "🔟",
+            ]
+
+        description = []
+        for x, option in enumerate(options):
+            description += "\n {} {}".format(reactions[x], option)
+        question += "\nPlusieurs votes par personne ? "
+        question += "oui" if multivote else "non"
+        embed = discord.Embed(title=question, description="".join(description))
+        react_message = await ctx.send(embed=embed)
+        for reaction in reactions[: len(options)]:
+            await react_message.add_reaction(reaction)
+        embed.set_footer(text="Poll ID: {}".format(react_message.id))
+        await react_message.edit_message(embed=embed)
+
+    @commands.hybrid_command(
+        name="tally",
+        description="Compte les votes d'un sondage",
+    )
+    async def tally(self, ctx, id=None):
+        poll_message = await ctx.channel.fetch_message(id)
+        user_can_multivote = poll_message.embeds[0].title[-3:] == "oui"
+        print(user_can_multivote)
+        embed = poll_message.embeds[0]
+
+        # On retire la dernière phrase, qui fait 36 caractères
+        title = embed.title[:-36]
+
+        splitted_description = embed.description.split("\n")
+        splitted_description = [x.strip() for x in splitted_description]
+        if splitted_description[0][:3] == "1️⃣":
+            # Liste à nombre
+            opt_dict = {
+                reaction[:3]: reaction[3:].strip()
+                for reaction in splitted_description[:9]
+            }
+            if len(splitted_description) == 10:
+                # If there is a tenth option, split it differently (because it works that way 🤷)
+                opt_dict["🔟"] = splitted_description[9][2:].strip()
+        else:
+            # yes/no
+            opt_dict = {
+                reaction[:1]: reaction[2:].strip()
+                for reaction in splitted_description
+            }
+
+        voters = [
+            self.bot.user.id
+        ]  # add the bot's ID to the list of voters to exclude it's votes
+
+        tally = {x: 0 for x in opt_dict.keys()}
+        for reaction in poll_message.reactions:
+            if reaction.emoji in opt_dict.keys():
+                reactors = [user async for user in reaction.users()]
+                for reactor in reactors:
+                    if reactor.id not in voters:
+                        tally[reaction.emoji] += 1
+                        if not user_can_multivote:
+                            voters.append(reactor.id)
+        output = f"Results of the poll for '{title}':\n" + "\n".join(
+            [
+                "{}: {}".format(opt_dict[key], tally[key])
+                for key in tally.keys()
+            ]
+        )
+        await ctx.channel.send(output)
 
 
 async def setup(bot) -> None:
