33029926f6150d8aeb3609a7953641523ff591aa
Augustin BAR <baraugustin26@gmail.com>
2023-06-12 18:40:03 +0200

    fetching messages at the end of the forum

--- discord_bot/modules/ReportModule.py
+++ discord_bot/modules/ReportModule.py
@@ -4,11 +4,56 @@ Module qui servira à faire les rapports en fin de journée ou de forum
 import logging
 
 
-async def generate_daily_report() -> None:
+async def generate_daily_report(forum) -> None:
     logging.info("Generating daily report...")
     logging.info("Done!")
 
 
-async def generate_forum_report() -> None:
+async def generate_forum_report(forum) -> None:
     logging.info("Generating forum report...")
-    logging.info("Done!")
+
+    logging.info("Fecthing all messages")
+
+    # get guild from guild id
+    guild = forum.bot.get_guild(forum.server_id)
+
+    # get all channels
+    channels = guild.text_channels
+
+    # If there are channel we're not supposed to log, remove them
+    not_log_channels = forum.get_data("not_log_channels")
+
+    if not_log_channels != {}:
+        channels = [
+            channel
+            for channel in channels
+            if (
+                str(channel.id) not in not_log_channels
+                and channel.id not in not_log_channels
+            )
+        ]
+
+    # fetch all messages
+    messages = []
+    for channel in channels:
+        channel_messages = [
+            message async for message in channel.history(limit=None)
+        ]
+        messages.extend(channel_messages)
+
+    # sort messages by date
+    messages.sort(key=lambda x: x.created_at)
+
+    # write messages in a file
+    with open("forum_report.txt", "w", encoding="utf-8") as f:
+        for message in messages:
+            try:
+                # write in utf-8
+                f.write(f"{message.author.name} : {message.content}\n")
+            except Exception as e:
+                logging.error(
+                    f"Error while writing message {message.id} : {e}"
+                )
+                continue
+
+    logging.info("Forum report done!")
